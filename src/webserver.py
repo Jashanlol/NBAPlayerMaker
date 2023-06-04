8728758d90a0e473e1c03b899af8d64efd0da0da
Jashan <37347402+Jashanlol@users.noreply.github.com>
2023-06-03 17:51:17 -0700

    Set up webserver
    Webserver will send requests from JS to python with the name of the player input on the website

---
+++ src/webserver.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def serveHTML():
+    return render_template('nba.html')
+
+@app.route("/generate", methods=['POST'])
+def generate():
+    request_data = request.get_json()
+    player_name = request_data.get("playerName")
+    
+    return player_name
+
+if __name__ == "__main__":
+    app.run(port=8080)
