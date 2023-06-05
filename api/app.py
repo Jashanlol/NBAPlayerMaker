ff658a3890b577fbd0127acae0f38ac91264ef93
Jashan <37347402+Jashanlol@users.noreply.github.com>
2023-06-04 21:09:42 -0700

    use html forms instead of input and buttons

--- api/app.py
+++ api/app.py
@@ -6,9 +6,7 @@ app = Flask(__name__)
 def serveHTML():
     return render_template('nba.html')
 
-@app.route("/generate", methods=['POST'])
+@app.route("/myplayer", methods=['POST'])
 def generate():
-    request_data = request.get_json()
-    player_name = request_data.get("playerName")
-    
-    return player_name
+    player_name = request.form["playerName"]
+    return f"Generating player... {player_name}"
